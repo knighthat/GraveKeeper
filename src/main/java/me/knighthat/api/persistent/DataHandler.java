ffcdc9b4bda7d50c0479fa5c31a906977a3d39c1
knighthat <knightchanelgaming@gmail.com>
2023-04-27 14:55:37 -0500

    added PDT and its handler

---
+++ src/main/java/me/knighthat/api/persistent/DataHandler.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2023. Knight Hat
+ * All rights reserved.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ *  and associated documentation files (the "Software"), to deal in the Software without restriction,
+ *  including without limitation the rights to use, copy, modify, merge, publish, distribute,sublicense, and/or sell copies of the Software,
+ *   and to permit persons to whom the Software is furnished to do so,
+ *   subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ *  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ *  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package me.knighthat.api.persistent;
+
+import lombok.NonNull;
+import me.knighthat.plugin.GraveKeeper;
+import me.knighthat.plugin.grave.Grave;
+import org.bukkit.NamespacedKey;
+import org.bukkit.block.TileState;
+import org.bukkit.entity.Player;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DataHandler {
+
+    private static final @NonNull GraveKeeper PLUGIN = JavaPlugin.getPlugin(GraveKeeper.class);
+
+    public static final @NonNull NamespacedKey KEY = new NamespacedKey(PLUGIN, PLUGIN.getDescription().getName());
+    private static final @NonNull PersistentDataType<byte[], Grave[]> CHEST = new GraveDataType();
+    private static final @NonNull PersistentDataType<String, String> ID = PersistentDataType.STRING;
+
+    /*
+
+        GRAVE
+
+    */
+    public static @NonNull String pull(@NonNull TileState state) {
+        PersistentDataContainer container = state.getPersistentDataContainer();
+        return container.has(KEY, ID) ? container.get(KEY, ID) : "";
+    }
+
+    public static void push(@NonNull TileState state, @NonNull String id) {
+        state.getPersistentDataContainer().set(KEY, ID, id);
+        state.update();
+    }
+
+
+    /*
+
+        PLAYER
+
+    */
+    public static Grave @NonNull [] pull(@NonNull Player player) {
+        PersistentDataContainer container = player.getPersistentDataContainer();
+        return container.has(KEY, CHEST) ? container.get(KEY, CHEST) : new Grave[0];
+    }
+
+    public static void push(@NonNull Player player, @NonNull Grave grave) {
+        Grave[] graves = pull(player);
+        Grave[] newArr = Arrays.copyOf(graves, graves.length + 1);
+        newArr[graves.length] = grave;
+
+        set(player, newArr);
+    }
+
+    public static @NonNull Grave get(@NonNull Player player, @NonNull String id) {
+        return Arrays.stream(pull(player))
+                .filter(c -> c.getId().equals(id))
+                .findFirst()
+                .orElse(new Grave());
+    }
+
+    public static void set(@NonNull Player player, @NonNull Grave[] graves) {
+        player.getPersistentDataContainer().set(KEY, CHEST, graves);
+    }
+
+    public static void remove(@NonNull Player player, @NonNull String id) {
+        Set<Grave> graves = new HashSet<>(List.of(pull(player)));
+        graves.removeIf(c -> c.getId().equals(id));
+
+        set(player, graves.toArray(Grave[]::new));
+    }
+
+    public static void reset(@NonNull Player player) {
+        for (Grave grave : pull(player))
+            grave.remove();
+
+        player.getPersistentDataContainer().remove(KEY);
+        set(player, new Grave[0]);
+    }
+
+}
---
+++ src/main/java/me/knighthat/api/persistent/GraveDataType.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2023. Knight Hat
+ * All rights reserved.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ *  and associated documentation files (the "Software"), to deal in the Software without restriction,
+ *  including without limitation the rights to use, copy, modify, merge, publish, distribute,sublicense, and/or sell copies of the Software,
+ *   and to permit persons to whom the Software is furnished to do so,
+ *   subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ *  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ *  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package me.knighthat.api.persistent;
+
+import me.knighthat.debugger.Debugger;
+import me.knighthat.plugin.grave.Grave;
+import org.bukkit.persistence.PersistentDataAdapterContext;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+
+/**
+ * Allows server to write and read "Grave Object" as bytes from NBT
+ */
+public class GraveDataType implements PersistentDataType<byte[], Grave[]> {
+
+    @Override
+    public @NotNull Class<byte[]> getPrimitiveType() {
+        return byte[].class;
+    }
+
+    @Override
+    public @NotNull Class<Grave[]> getComplexType() {
+        return Grave[].class;
+    }
+
+    @Override
+    public byte @NotNull [] toPrimitive(Grave @NotNull [] graves, @NotNull PersistentDataAdapterContext persistentDataAdapterContext) {
+        try {
+            ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+            BukkitObjectOutputStream bOutStream = new BukkitObjectOutputStream(outStream);
+            bOutStream.writeObject(graves);
+            bOutStream.flush();
+
+            return outStream.toByteArray();
+        } catch (IOException e) {
+            Debugger.err("Couldn't convert Grave Object to Bytes", e.getLocalizedMessage());
+            e.printStackTrace();
+
+            return new byte[0];
+        }
+    }
+
+    @Override
+    public Grave @NotNull [] fromPrimitive(byte @NotNull [] bytes, @NotNull PersistentDataAdapterContext persistentDataAdapterContext) {
+        try {
+            ByteArrayInputStream inStream = new ByteArrayInputStream(bytes);
+            BukkitObjectInputStream bInStream = new BukkitObjectInputStream(inStream);
+
+            return (Grave[]) bInStream.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            Debugger.err("Couln't convert Bytes to Grave Object", e.getLocalizedMessage());
+            e.printStackTrace();
+
+            return new Grave[0];
+        }
+    }
+}
