68a464b5b6b128e194304eb3beae651d8f221db4
knighthat <knightchanelgaming@gmail.com>
2023-04-27 18:50:38 -0500

    update register event handlers

--- src/main/java/me/knighthat/plugin/GraveKeeper.java
+++ src/main/java/me/knighthat/plugin/GraveKeeper.java
@@ -1,16 +1,43 @@
-package me.knighthat.gravekeeper;
+/*
+ * Copyright (c) 2023. Knight Hat
+ * All rights reserved.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ *  and associated documentation files (the "Software"), to deal in the Software without restriction,
+ *  including without limitation the rights to use, copy, modify, merge, publish, distribute,sublicense, and/or sell copies of the Software,
+ *   and to permit persons to whom the Software is furnished to do so,
+ *   subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ *  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ *  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
-import me.knighthat.gravekeeper.event.EventController;
+package me.knighthat.plugin;
+
+import me.knighthat.debugger.Debugger;
+import me.knighthat.plugin.event.EventController;
+import me.knighthat.plugin.file.MessageFile;
+import me.knighthat.plugin.message.Messenger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class GraveKeeper extends JavaPlugin {
+
+
+    {
+        Debugger.LOGGER = this.getSLF4JLogger();
+        Messenger.FILE = new MessageFile(this);
+    }
 
     @Override
     public void onEnable() {
 
         // Register event handler to Server
         getServer().getPluginManager().registerEvents(new EventController(), this);
-
-
     }
 }
