ceb87306129ff2905df6530df7fd5ade52da680d
knighthat <knightchanelgaming@gmail.com>
2023-05-01 12:25:51 -0500

    updated package path

--- src/main/java/me/knighthat/plugin/grave/Content.java
+++ src/main/java/me/knighthat/plugin/grave/Content.java
@@ -18,7 +18,7 @@
  *  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-package me.knighthat.plugin.deprecated.grave;
+package me.knighthat.plugin.grave;
 
 import lombok.NonNull;
 import lombok.Setter;
