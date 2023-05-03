8c21810a74e05773954905e6d23ff9df2e0f2dc1
knighthat <knightchanelgaming@gmail.com>
2023-05-02 22:29:17 -0500

    auto string replacement on file get & log on fail

--- src/main/java/me/knighthat/api/file/PluginFile.java
+++ src/main/java/me/knighthat/api/file/PluginFile.java
@@ -21,7 +21,12 @@
 package me.knighthat.api.file;
 
 import lombok.NonNull;
+import me.knighthat.debugger.Debugger;
 import me.knighthat.plugin.GraveKeeper;
+import me.knighthat.plugin.instance.Grave;
+
+import java.text.MessageFormat;
+import java.util.Map;
 
 public abstract class PluginFile extends FileTemplate {
 
@@ -29,11 +34,94 @@ public abstract class PluginFile extends FileTemplate {
         super(plugin, fileName);
     }
 
+    /**
+     * Applies placeholders with template %placeholder:[value]
+     *
+     * @param original     String to be replaced
+     * @param replacements Pair of %placeholder:[value]
+     * @return new string with %placeholder replaced by [value]
+     */
+    @NonNull String apply(@NonNull String original, @NonNull Map<String, String> replacements) {
+        String message = original;
+        for (Map.Entry<String, String> entry : replacements.entrySet())
+            message = message.replace(entry.getKey(), entry.getValue());
+
+        return message;
+    }
+
+    /**
+     * Get string from path and return new string
+     * replaced by replacements
+     *
+     * @param path         Where to get string
+     * @param replacements Pair of %placeholder:[value]
+     * @return new string with %placeholder replaced by [value]
+     */
+    public @NonNull String string(@NonNull String path, @NonNull Map<String, String> replacements) {
+        String original = this.string(path);
+        return this.apply(original, replacements);
+    }
+
+    /**
+     * Get string from path and return new string
+     * replaced by every pair of %placeholder:[value]
+     * provided by Grave.
+     *
+     * @param path  Where to get string
+     * @param grave Instance offers replacements
+     * @return new string with %placeholder replaced by [value]
+     */
+    public @NonNull String string(@NonNull String path, @NonNull Grave grave) {
+        return this.string(path, grave.replacements());
+    }
+
+    /**
+     * Get string from path<br>
+     * Returns "404" if path's not exist
+     * or path doesn't contain string.
+     * Plus, log a warning to console
+     *
+     * @param path Where to get string
+     * @return A string from file
+     */
     public @NonNull String string(@NonNull String path) {
+        if (contains(path) && !get().isString(path)) {
+            String message = "{0} is not a string!";
+            Debugger.warn(MessageFormat.format(message, path));
+        }
         return get().getString(path, "404");
     }
 
+    /**
+     * Get integer from path.<br>
+     * Returns "0" if path's not exist
+     * or path doesn't contain integer.
+     * Plus, log a warning to console
+     *
+     * @param path Where to get int
+     * @return A string from file
+     */
     public int integer(@NonNull String path) {
+        if (contains(path) && !get().isInt(path)) {
+            String message = "{0} is not an integer!";
+            Debugger.warn(MessageFormat.format(message, path));
+        }
         return get().getInt(path, 0);
+    }
+
+    /**
+     * Checks if path exists in file.<br>
+     * Log a warning to console
+     * if path does not exist.
+     *
+     * @param path Where to get object
+     * @return If path exists in file
+     */
+    private boolean contains(@NonNull String path) {
+        if (!get().contains(path)) {
+            String message = "{0} not found in {1}! (404)";
+            Debugger.warn(MessageFormat.format(message, path, super.getFileName()));
+        }
+        return get().contains(path);
     }
 }
